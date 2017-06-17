83eb25febf7155d66a26c9f7b73fa40a5c145561
Maicon Moretto <631410161@fspoaeduc.com.br>
2017-06-16 21:05:15 -0300

    adicionado projeto

--- src/cantinamonetizada/buffer.java
+++ src/cantinamonetizada/buffer.java
@@ -26,6 +26,7 @@ public class buffer {
         ByteBuffer buf = ByteBuffer.allocate(48);
 
         int bytesRead = inChannel.read(buf); //read into buffer.
+
         while (bytesRead != -1) {
 
             buf.flip();  //make buffer ready for read
@@ -37,6 +38,7 @@ public class buffer {
             buf.clear(); //make buffer ready for writing
             bytesRead = inChannel.read(buf);
         }
+     
 
         aFile.close();
     }
--- src/cantinamonetizada/controleOperacoes.java
+++ src/cantinamonetizada/controleOperacoes.java
@@ -6,6 +6,11 @@
 package cantinamonetizada;
 
 import cantinamonetizada.buffer;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 
 /**
  *
@@ -16,10 +21,32 @@ public class controleOperacoes {
     public controleOperacoes() {
     }
 
-    public boolean verificaSaldo(int matricula) {
+    public boolean verificaSaldo(int matricula) throws IOException {
         buffer b = new buffer();
-
+        b.testebuffer();
         return true;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        RandomAccessFile aFile = new RandomAccessFile("data/nio-data.txt", "rw");
+        FileChannel inChannel = aFile.getChannel();
+
+//create buffer with capacity of 48 bytes
+        ByteBuffer buf = ByteBuffer.allocate(48);
+
+        int bytesRead = inChannel.read(buf); //read into buffer.
+        while (bytesRead != -1) {
+
+            buf.flip();  //make buffer ready for read
+
+            while (buf.hasRemaining()) {
+                System.out.print((char) buf.get()); // read 1 byte at a time
+            }
+
+            buf.clear(); //make buffer ready for writing
+            bytesRead = inChannel.read(buf);
+        }
+        aFile.close();
     }
 
 }
